757121c01aace323f5ab583099a2d328cade9605
Liviu Ionescu <ilg@livius.net>
2019-07-07 00:32:05 +0300

    git-clone: update source url

--- scripts/git-clone.sh
+++ scripts/git-clone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 rm -rf "${HOME}/Downloads/qemu-arm-xpack.git"
-git clone --recurse-submodules https://github.com/gnu-mcu-eclipse/qemu-arm-xpack.git "${HOME}/Downloads/qemu-arm-xpack.git"
+git clone --recurse-submodules https://github.com/xpack-dev-tools/qemu-arm-xpack.git "${HOME}/Downloads/qemu-arm-xpack.git"
