90f32c5dcd0d7396a252c227f100ab69a4293e63
Liviu Ionescu <ilg@livius.net>
2019-07-07 00:17:45 +0300

    git-clone: use qemu-arm-xpack

--- scripts/git-clone.sh
+++ scripts/git-clone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rm -rf "${HOME}/Downloads/qemu-build.git"
-git clone --recurse-submodules https://github.com/gnu-mcu-eclipse/qemu-build.git "${HOME}/Downloads/qemu-build.git"
+rm -rf "${HOME}/Downloads/qemu-arm-xpack.git"
+git clone --recurse-submodules https://github.com/gnu-mcu-eclipse/qemu-arm-xpack.git "${HOME}/Downloads/qemu-arm-xpack.git"
