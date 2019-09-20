7d4be339ea5689ab7586217e3a3379aa24a0294b
Liviu Ionescu <ilg@livius.net>
2019-09-20 18:38:20 +0300

    Fix 'stripping' typo

--- scripts/common-apps-functions-source.sh
+++ scripts/common-apps-functions-source.sh
@@ -263,7 +263,7 @@ function strip_binaries()
       xbb_activate
 
       echo
-      echo "Striping binaries..."
+      echo "Stripping binaries..."
 
       if [ "${TARGET_PLATFORM}" == "win32" ]
       then
