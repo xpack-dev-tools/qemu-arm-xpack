8d56aea06e10644f1bea09172f17c310994dbd5a
Liviu Ionescu <ilg@livius.net>
2022-05-03 19:55:41 +0300

    common-versions: update for 7.0.0

--- scripts/common-versions-source.sh
+++ scripts/common-versions-source.sh
@@ -166,178 +166,154 @@ function build_versions()
 
         build_qemu_legacy "${QEMU_LEGACY_VERSION}"
       fi
-
     )
-
   # ---------------------------------------------------------------------------
-  if [[ "${RELEASE_VERSION}" =~ 6\.2\.*-* ]]
+  elif [[ "${RELEASE_VERSION}" =~ 6\.2\.0-[123] ]]
   then
+    (
+      xbb_activate
 
-    if [[ "${RELEASE_VERSION}" =~ 6\.2\.0-[123] ]]
-    then
-      (
-        xbb_activate
+      # For now use the same code as the RISC-V build.
+      # QEMU_GIT_BRANCH=${QEMU_GIT_BRANCH:-"xpack-develop"}
+      # QEMU_GIT_COMMIT=${QEMU_GIT_COMMIT:-"v${RELEASE_VERSION}-xpack-arm"}
+      QEMU_GIT_BRANCH=${QEMU_GIT_BRANCH:-"branding-patch"}
+      QEMU_GIT_COMMIT=${QEMU_GIT_COMMIT:-"8cbbe264ab0e738263759404d0e1e91090046267"}
 
-        # For now use the same code as the RISC-V build.
-        # QEMU_GIT_BRANCH=${QEMU_GIT_BRANCH:-"xpack-develop"}
-        # QEMU_GIT_COMMIT=${QEMU_GIT_COMMIT:-"v${RELEASE_VERSION}-xpack-arm"}
-        QEMU_GIT_BRANCH=${QEMU_GIT_BRANCH:-"branding-patch"}
-        QEMU_GIT_COMMIT=${QEMU_GIT_COMMIT:-"8cbbe264ab0e738263759404d0e1e91090046267"}
+      QEMU_LEGACY_VERSION="${QEMU_LEGACY_VERSION:-"2.8.0-14"}"
+      QEMU_LEGACY_GIT_COMMIT="${QEMU_LEGACY_GIT_COMMIT:-"v${QEMU_LEGACY_VERSION}-xpack-legacy"}"
+      QEMU_LEGACY_GIT_PATCH="qemu-2.8.0.git-patch"
 
-        QEMU_LEGACY_VERSION="${QEMU_LEGACY_VERSION:-"2.8.0-14"}"
-        QEMU_LEGACY_GIT_COMMIT="${QEMU_LEGACY_GIT_COMMIT:-"v${QEMU_LEGACY_VERSION}-xpack-legacy"}"
-        QEMU_LEGACY_GIT_PATCH="qemu-2.8.0.git-patch"
+      build_zlib "1.2.11"
 
-        build_zlib "1.2.11"
+      if [ "${TARGET_PLATFORM}" != "win32" ]
+      then
+        build_bzip2 "1.0.8"
+      fi
 
-        if [ "${TARGET_PLATFORM}" != "win32" ]
-        then
-          build_bzip2 "1.0.8"
-        fi
+      build_zstd "1.5.0"
 
-        build_zstd "1.5.0"
+      # required by nettle
+      build_gmp "6.2.1"
 
-        # required by nettle
-        build_gmp "6.2.1"
+      build_libpng "1.6.37"
+      build_jpeg "9d"
 
-        build_libpng "1.6.37"
-        build_jpeg "9d"
+      if [ "${TARGET_PLATFORM}" != "win32" ]
+      then
+        build_libxml2 "2.9.11"
+      fi
 
-        if [ "${TARGET_PLATFORM}" != "win32" ]
-        then
-          build_libxml2 "2.9.11"
-        fi
+      # required by glib
+      build_libiconv "1.16"
 
-        # required by glib
-        build_libiconv "1.16"
+      if false # [ "${TARGET_PLATFORM}" == "darwin" ]
+      then
+        : # On macOS use Cocoa.
+      else
+        build_sdl2 "2.0.18"
+        build_sdl2_image "2.0.5"
+      fi
 
-        if false # [ "${TARGET_PLATFORM}" == "darwin" ]
-        then
-          : # On macOS use Cocoa.
-        else
-          build_sdl2 "2.0.18"
-          build_sdl2_image "2.0.5"
-        fi
+      # required by glib
+      build_libffi "3.4.2"
 
-        # required by glib
-        build_libffi "3.4.2"
+      # required by glib
+      build_gettext "0.21"
 
-        # required by glib
-        build_gettext "0.21"
+      # TODO "2.70.2" (meson)
+      build_glib "2.56.4" #
 
-        # TODO "2.70.2" (meson)
-        build_glib "2.56.4" #
+      # Not toghether with nettle.
+      # build_libgpg_error "1.43"
+      # build_libgcrypt "1.9.4"
 
-        # Not toghether with nettle.
-        # build_libgpg_error "1.43"
-        # build_libgcrypt "1.9.4"
+      # https://github.com/Homebrew/homebrew-core/blob/master/Formula/gnutls.rb
+      # gnutls
 
-        # https://github.com/Homebrew/homebrew-core/blob/master/Formula/gnutls.rb
-        # gnutls
+      # libslirp
 
-        # libslirp
+      # libcurl
 
-        # libcurl
+      if [ "${TARGET_PLATFORM}" != "win32" ]
+      then
+        # required by libssh
+        build_openssl "1.1.1l"
 
-        if [ "${TARGET_PLATFORM}" != "win32" ]
-        then
-          # required by libssh
-          build_openssl "1.1.1l"
+        build_libssh "0.9.6"
 
-          build_libssh "0.9.6"
+        build_ncurses "6.3"
+      fi
 
-          build_ncurses "6.3"
-        fi
+      if [ "${TARGET_PLATFORM}" == "win32" ]
+      then
+        # TODO: check if QEMU can use it or something else is needed.
+        build_libusb_w32 "1.2.6.0"
+      else
+        build_libusb "1.0.24"
+      fi
 
-        if [ "${TARGET_PLATFORM}" == "win32" ]
-        then
-          # TODO: check if QEMU can use it or something else is needed.
-          build_libusb_w32 "1.2.6.0"
-        else
-          build_libusb "1.0.24"
-        fi
+      build_lzo "2.10"
 
-        build_lzo "2.10"
+      build_nettle "3.7.3"
 
-        build_nettle "3.7.3"
+      build_pixman "0.40.0"
 
-        build_pixman "0.40.0"
+      # https://github.com/Homebrew/homebrew-core/blob/master/Formula/snappy.rb
+      # snappy - Compression/decompression library aiming for high speed
 
-        # https://github.com/Homebrew/homebrew-core/blob/master/Formula/snappy.rb
-        # snappy - Compression/decompression library aiming for high speed
+      if [ "${TARGET_PLATFORM}" != "win32" ]
+      then
+        # required by vde
+        build_libpcap "1.10.1"
+        build_vde "2.3.2"
+      fi
 
-        if [ "${TARGET_PLATFORM}" != "win32" ]
-        then
-          # required by vde
-          build_libpcap "1.10.1"
-          build_vde "2.3.2"
-        fi
+      # Build legacy qemu-system-gnuarmeclipse is not available
+      # on Apple Silicon.
+      if [ "${TARGET_PLATFORM}" == "darwin" -a "${TARGET_ARCH}" == "arm64" ]
+      then
+        : # Skip.
+      else
+        : # build_qemu_legacy "${QEMU_LEGACY_VERSION}"
+      fi
 
-        # Build legacy qemu-system-gnuarmeclipse is not available
-        # on Apple Silicon.
-        if [ "${TARGET_PLATFORM}" == "darwin" -a "${TARGET_ARCH}" == "arm64" ]
-        then
-          : # Skip.
-        else
-          : # build_qemu_legacy "${QEMU_LEGACY_VERSION}"
-        fi
-
-        if [ "${RELEASE_VERSION}" == "6.2.0-1" ]
-        then
-          export ENABLE_QEMU_SDL="y"
-        fi
-        # After legacy, to prevent it overriding newer files.
-        build_qemu "${QEMU_VERSION}" "arm"
-        trim_qemu_arm
-      )
-
-    # -------------------------------------------------------------------------
-    else
-      echo "Unsupported version ${RELEASE_VERSION}."
-      exit 1
-    fi
-
-    # -------------------------------------------------------------------------
-  elif [[ "${RELEASE_VERSION}" =~ 2\.8\.0-* ]]
+      if [ "${RELEASE_VERSION}" == "6.2.0-1" ]
+      then
+        export ENABLE_QEMU_SDL="y"
+      fi
+      # After legacy, to prevent it overriding newer files.
+      build_qemu "${QEMU_VERSION}" "arm"
+      trim_qemu_arm
+    )
+  # ---------------------------------------------------------------------------
+  elif [[ "${RELEASE_VERSION}" =~ 2\.8\.0-13 ]]
   then
+    (
+      xbb_activate
 
-    # -------------------------------------------------------------------------
+      QEMU_LEGACY_GIT_COMMIT=${QEMU_LEGACY_GIT_COMMIT:-"b1ab9f0b322a905f8c5983692e800472a6556323"}
 
-    if [[ "${RELEASE_VERSION}" =~ 2\.8\.0-13 ]]
-    then
-      (
-        xbb_activate
+      QEMU_LEGACY_GIT_PATCH="qemu-2.8.0.git-patch"
 
-        QEMU_LEGACY_GIT_COMMIT=${QEMU_LEGACY_GIT_COMMIT:-"b1ab9f0b322a905f8c5983692e800472a6556323"}
+      build_zlib "1.2.11"
 
-        QEMU_LEGACY_GIT_PATCH="qemu-2.8.0.git-patch"
+      build_libpng "1.6.36"
+      build_jpeg "9b"
+      build_libiconv "1.15"
 
-        build_zlib "1.2.11"
+      build_sdl2 "2.0.9"
+      build_sdl2_image "2.0.4"
 
-        build_libpng "1.6.36"
-        build_jpeg "9b"
-        build_libiconv "1.15"
+      build_libffi "3.2.1"
 
-        build_sdl2 "2.0.9"
-        build_sdl2_image "2.0.4"
+      # in certain configurations it requires libxml2 on windows
+      build_gettext "0.21" # "0.19.8.1"
+      build_glib "2.56.4"
+      build_pixman "0.40.0" # "0.38.0"
 
-        build_libffi "3.2.1"
-
-        # in certain configurations it requires libxml2 on windows
-        build_gettext "0.21" # "0.19.8.1"
-        build_glib "2.56.4"
-        build_pixman "0.40.0" # "0.38.0"
-
-        build_qemu_legacy "${QEMU_LEGACY_VERSION}"
-      )
-
-    # -------------------------------------------------------------------------
-    else
-      echo "Unsupported version ${RELEASE_VERSION}."
-      exit 1
-    fi
-
-    # -------------------------------------------------------------------------
+      build_qemu_legacy "${QEMU_LEGACY_VERSION}"
+    )
+  # -------------------------------------------------------------------------
   else
     echo "Unsupported version ${RELEASE_VERSION}."
     exit 1
