3a1bc6db88dd18c04677c6913a9933b130fb9699
Liviu Ionescu <ilg@livius.net>
2023-01-28 20:36:02 +0200

    versioning.sh: use versioning functions

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -11,9 +11,9 @@
 
 function application_build_versioned_components()
 {
-  export XBB_QEMU_VERSION="$(echo "${XBB_RELEASE_VERSION}" | sed -e 's|-.*||')"
+  export XBB_QEMU_VERSION="$(xbb_strip_version_pre_release "${XBB_RELEASE_VERSION}")"
 
-  # Keep them in sync with combo archive content.
+  # Keep them in sync with the combo archive content.
   if [[ "${XBB_RELEASE_VERSION}" =~ 7[.]1[.]0-1 ]]
   then
     # -------------------------------------------------------------------------
