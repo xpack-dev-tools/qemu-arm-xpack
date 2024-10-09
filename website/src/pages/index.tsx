fa024dcd3ff4f6ebe252ded87f6f38b50f8f6900
Liviu Ionescu <ilg@livius.net>
2024-10-10 01:33:14 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from xPack Binary Development Tools`}
-      description="A binary xPack with the QEMU Arm executables">
+      description="A binary package with the QEMU Arm executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
