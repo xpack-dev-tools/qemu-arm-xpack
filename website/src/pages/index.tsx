b11abc33b69a4646bbc7562d4d02fa0e077568b0
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:34:11 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to qemu-arm`}
+      title="Welcome!"
       description="A binary package with the qemu-arm executables">
       <HomepageHeader />
       <main>
