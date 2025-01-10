7412fcc7ec3fb8448b3b1cd0d4935587c125f94f
Liviu Ionescu <ilg@livius.net>
2025-01-10 13:21:11 +0200

    website: updates

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -89,7 +89,8 @@ console.log('customFields: ' + util.inspect(customFields));
 // ----------------------------------------------------------------------------
 
 const config: Config = {
-  title: 'xPack QEMU Arm',
+  title: 'xPack QEMU Arm' +
+    ((process.env.DOCUSAURUS_IS_PREVIEW === 'true') ? ' (preview)' : ''),
   tagline: 'A binary distribution of QEMU Arm',
   // Explicitly set in headTags.
   // favicon: '/img/favicon.ico',
@@ -260,7 +261,7 @@ const config: Config = {
       items: [
         {
           to: '/',
-          // label: 'Home',
+          label: 'qemu-arm',
           className: 'header-home-link',
           position: 'left'
         },
