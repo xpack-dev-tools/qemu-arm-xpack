d770e3bb864b2b8fb9772dc1e6f84e88aec429a0
Liviu Ionescu <ilg@livius.net>
2025-01-21 00:31:17 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -34,7 +34,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @xpack-dev-tools/qemu-arm@{customField('version')} --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @xpack-dev-tools/qemu-arm@{customField('releaseVersion')} --verbose</InstallWithCopy>
         </div>
 
       </div>
