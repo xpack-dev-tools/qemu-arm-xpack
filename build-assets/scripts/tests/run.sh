9070f00a8db7abe406c79dc65330deb02a47b536
Liviu Ionescu <ilg@livius.net>
2025-10-08 22:01:25 +0300

    remove legacy test

--- build-assets/scripts/tests/run.sh
+++ build-assets/scripts/tests/run.sh
@@ -21,13 +21,6 @@ function tests_run_all()
   local test_bin_path="$1"
 
   qemu_arm_test "${test_bin_path}"
-
-  if [ "${XBB_HOST_PLATFORM}" == "darwin" -a "${XBB_HOST_ARCH}" == "arm64" ]
-  then
-    : # Not available on Apple Silicon.
-  else
-    qemu_arm_legacy_test "${test_bin_path}"
-  fi
 }
 
 # -----------------------------------------------------------------------------
